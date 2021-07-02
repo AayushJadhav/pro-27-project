436e8b2abcc03f45bfdd2b676399ec079c6240dc
Aayush Jadhav <83593606+AayushJadhav@users.noreply.github.com>
2021-07-02 23:18:45 +0530

    Indented properly.

--- sketch.js
+++ sketch.js
@@ -22,40 +22,40 @@ function setup()
 	world = engine.world;
 
 	roofObject=new Roof(400,250,230,20);
-	
+
 	bob1 = new Bob(320, 575, 40);
 	bob2 = new Bob(360, 575, 40);
 	bob3 = new Bob(380, 575, 40);
 	bob4 = new Bob(400, 575, 40);
 	bob5 = new Bob(420, 575, 40);
-	
+
 	rope1 = new Rope(bob1.body, roofObject.body, -80, 0);
 	rope2 = new Rope(bob2.body, roofObject.body, -40, 0);
 	rope3 = new Rope(bob3.body, roofObject.body, 0, 0);
 	rope4 = new Rope(bob4.body, roofObject.body, 40, 0);
 	rope5 = new Rope(bob5.body, roofObject.body, 80, 0);
-	
+
 	Engine.run(engine);
 }
 
 
 function draw()
 {
-  rectMode(CENTER);
-  background(230);
-  roofObject.display();
+	rectMode(CENTER);
+	background(230);
+	roofObject.display();
 
-  rope1.display();
-  rope2.display();
-  rope3.display();
-  rope4.display();
-  rope5.display();
+	rope1.display();
+	rope2.display();
+	rope3.display();
+	rope4.display();
+	rope5.display();
 
-  bob1.display();
-  bob2.display();
-  bob3.display();
-  bob4.display();
-  bob5.display();
+	bob1.display();
+	bob2.display();
+	bob3.display();
+	bob4.display();
+	bob5.display();
 }
 
 function keyPressed()
